5108fb48b2b59190b1fee4eb8da79d28fd5a07fb
githublms <lms_ajdxwz@163.com>
2020-05-26 13:00:11 +0800

    创建了分支

--- src/main/java/com/lms/git/Hello.java
+++ src/main/java/com/lms/git/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello ,git");
-		System.out.println("更新1");
+		System.out.println("创建了分支dev");
+		
+		
 		
 	}
 
