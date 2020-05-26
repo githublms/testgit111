b04650ab58c61d32e7203ac8809a286375e78312
githublms <37056964+githublms@users.noreply.github.com>
2020-05-26 15:16:36 +0800

    在线添加内容

--- src/main/java/com/lms/git/Hello.java
+++ src/main/java/com/lms/git/Hello.java
@@ -4,13 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello ,git");
-		System.out.println("创建了分支dev");
-		System.out.println("创建了测试的分支");
-		System.out.println("主干添加");
-		System.out.println("创建了分支dev2");
-		System.out.println("dev分支添加2");
-		System.out.println("新增内容");
-		
+		System.out.println("鍒涘缓浜嗗垎鏀痙ev");
+		System.out.println("鍒涘缓浜嗘祴璇曠殑鍒嗘敮");
+		System.out.println("涓诲共娣诲姞");
+		System.out.println("鍒涘缓浜嗗垎鏀痙ev2");
+		System.out.println("dev鍒嗘敮娣诲姞2");
+		System.out.println("鏂板鍐呭");
+		System.out.println("鍦ㄧ嚎娣诲姞鍐呭");
 		
 	}
 
