32f503b359e81467c795d6d7793d4d9fcb0d7c50
githublms <lms_ajdxwz@163.com>
2020-05-26 14:02:54 +0800

    分支添加2

--- src/main/java/com/lms/git/Hello.java
+++ src/main/java/com/lms/git/Hello.java
@@ -5,9 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello ,git");
 		System.out.println("创建了分支dev");
-		
-		
-		System.out.println("dev分支添加");
+		System.out.println("创建了分支dev2");
+		System.out.println("dev分支添加2");
 		
 	}
 
